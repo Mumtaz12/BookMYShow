2df482544af137788a3933a64d6133bf691f719a
Mohammad  Mumtaz <41723154+Mumtaz12@users.noreply.github.com>
2023-03-23 23:46:05 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    
+    extend: {
+      colors: {litgray : '#303243',
+              litblue  : '#2B3149', },
+    },
+  },
+  plugins: [],
+}
